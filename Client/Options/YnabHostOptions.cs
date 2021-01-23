978c123400590e206e4bcb3bae4d437d80e4d20b
Jim Yarbro <nrrdio@outlook.com>
2021-01-23 13:25:02 +0100

    Moved service configuration into Client project

---
+++ Client/Options/YnabHostOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Models.Options {
+    public class YnabHostOptions {
+        public const string Section = "YNAB";
+
+        // This library targets the v1 API from https://api.youneedabudget.com/v1
+        public string EndPoint { get; set; }
+
+        // Get this from YNAB's site
+        public string AccessToken { get; set; }
+
+        // This is the first hash in the URL of a budget
+        public string BudgetId { get; set; }
+    }
+}
