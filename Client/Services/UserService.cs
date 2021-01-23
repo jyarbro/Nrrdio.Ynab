b322b5e7ce82f57642700fc7b072597675b9effe
Jim Yarbro <nrrdio@outlook.com>
2021-01-23 13:25:20 +0100

    First attempt at a user service.

---
+++ Client/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using App.Models.Options;
+using Microsoft.Extensions.Options;
+using Nrrdio.Ynab.Client.Models.Users;
+using System.Threading.Tasks;
+
+namespace Nrrdio.Ynab.Client.Services {
+    public class UserService {
+        string ResourceUrl { get; init; }
+        YnabApiService Ynab { get; init; }
+
+        public UserService(
+            IOptions<YnabHostOptions> hostOptions,
+            YnabApiService apiService
+        ) {
+            ResourceUrl = $"{hostOptions.Value.EndPoint}/user";
+            Ynab = apiService;
+        }
+
+        public async Task<User> GetUser() => await Ynab.GetRequest<User>(ResourceUrl);
+    }
+}
