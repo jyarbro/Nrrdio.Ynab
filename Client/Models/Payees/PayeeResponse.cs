91c446779f23f059d4e57df3f345b4c82445a574
Jim Yarbro <nrrdio@outlook.com>
2021-01-24 21:55:11 +0100

    example

--- Client/Models/Payees/PayeeResponse.cs
+++ Client/Models/Payees/PayeeResponse.cs
@@ -6,4 +6,19 @@
             public Payee Payee { get; set; }
         }
     }
+
+    /*
+     * Example:
+     * 
+        {
+          "data": {
+            "payee": {
+              "id": "asdf1234-asdf-1234-asdf-1234asdf1234",
+              "name": "Asdf 1234",
+              "transfer_account_id": null,
+              "deleted": false
+            }
+          }
+        }
+    */
 }
