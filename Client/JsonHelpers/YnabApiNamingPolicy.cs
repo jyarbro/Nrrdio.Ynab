d1e0f698af2ea536f3bdc17200a494d99c30674f
Jim Yarbro <nrrdio@outlook.com>
2021-01-24 22:03:05 +0100

    Prevent mistakes

--- Client/JsonHelpers/YnabApiNamingPolicy.cs
+++ Client/JsonHelpers/YnabApiNamingPolicy.cs
@@ -1,15 +1,15 @@
-﻿using System.Globalization;
-using System.Linq;
+﻿using System.Linq;
 using System.Text.Json;
 
 namespace Nrrdio.Ynab.Client.JsonHelpers {
-    public class SnakeToPascalNamingPolicy : JsonNamingPolicy {
-        public override string ConvertName(string name) => new CultureInfo("en-US", false).TextInfo.ToTitleCase(name).Replace("_", string.Empty);
+    // Probably unnecessary to keep this but it's here just in case.
+    //public class SnakeToPascalNamingPolicy : JsonNamingPolicy {
+    //    public override string ConvertName(string name) => new CultureInfo("en-US", false).TextInfo.ToTitleCase(name).Replace("_", string.Empty);
 
-        public static JsonSerializerOptions Options => new JsonSerializerOptions {
-            PropertyNamingPolicy = new SnakeToPascalNamingPolicy()
-        };
-    }
+    //    public static JsonSerializerOptions Options => new JsonSerializerOptions {
+    //        PropertyNamingPolicy = new SnakeToPascalNamingPolicy()
+    //    };
+    //}
 
     public class PascalToSnakeNamingPolicy : JsonNamingPolicy {
         public override string ConvertName(string name) => string.Concat(name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
