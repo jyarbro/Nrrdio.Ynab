d37293c1d5b12da17d407a3ddce9be0c837ac22c
Jim Yarbro <nrrdio@outlook.com>
2021-01-24 22:52:52 +0100

    Added converter for queries

---
+++ Client/JsonHelpers/IsoDateConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nrrdio.Ynab.Client.JsonHelpers {
+    public class IsoDateConverter : JsonConverter<DateTime> {
+        public override DateTime Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) {
+
+            var valueString = reader.GetString();
+            return Convert.ToDateTime(valueString);
+        }
+
+        public override void Write(
+            Utf8JsonWriter writer, 
+            DateTime value, 
+            JsonSerializerOptions options) {
+
+            writer.WriteStringValue(value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+        }
+    }
+}
