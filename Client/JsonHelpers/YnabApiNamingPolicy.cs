89f04c5a53a01c8ecfe76bb8373bb387e09bc319
Jim Yarbro <nrrdio@outlook.com>
2021-01-24 21:59:27 +0100

    Added opposite naming policy, and factory methods.

--- Client/JsonHelpers/YnabApiNamingPolicy.cs
+++ Client/JsonHelpers/YnabApiNamingPolicy.cs
@@ -1,8 +1,21 @@
 ﻿using System.Globalization;
+using System.Linq;
 using System.Text.Json;
 
 namespace Nrrdio.Ynab.Client.JsonHelpers {
-    class SnakeToPascalNamingPolicy : JsonNamingPolicy {
+    public class SnakeToPascalNamingPolicy : JsonNamingPolicy {
         public override string ConvertName(string name) => new CultureInfo("en-US", false).TextInfo.ToTitleCase(name).Replace("_", string.Empty);
+
+        public static JsonSerializerOptions Options => new JsonSerializerOptions {
+            PropertyNamingPolicy = new SnakeToPascalNamingPolicy()
+        };
+    }
+
+    public class PascalToSnakeNamingPolicy : JsonNamingPolicy {
+        public override string ConvertName(string name) => string.Concat(name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+
+        public static JsonSerializerOptions Options => new JsonSerializerOptions {
+            PropertyNamingPolicy = new PascalToSnakeNamingPolicy()
+        };
     }
 }
