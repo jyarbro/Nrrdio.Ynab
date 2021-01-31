7592432368c9befaca57f92ba9755685448791f2
Jim Yarbro <nrrdio@outlook.com>
2021-01-31 09:12:16 +0100

    example

--- Client/Models/Api/Transactions/TransactionsResponse.cs
+++ Client/Models/Api/Transactions/TransactionsResponse.cs
@@ -14,3 +14,57 @@ namespace Nrrdio.Ynab.Client.Models.Api.Transactions {
         }
     }
 }
+
+/*
+ * Example
+{
+  "data": {
+    "transactions": [
+      {
+        "id": "asdf1234-asdf-1234-asdf-1234asdf1234",
+        "date": "1999-12-31",
+        "amount": 20,
+        "memo": null,
+        "cleared": "reconciled",
+        "approved": true,
+        "flag_color": null,
+        "account_id": "asdf1234-asdf-1234-asdf-1234asdf1234",
+        "account_name": "Asdf 1234",
+        "payee_id": "asdf1234-asdf-1234-asdf-1234asdf1234",
+        "payee_name": "Asdf 1234",
+        "category_id": "asdf1234-asdf-1234-asdf-1234asdf1234",
+        "category_name": "Asdf 1234",
+        "transfer_account_id": null,
+        "transfer_transaction_id": null,
+        "matched_transaction_id": null,
+        "import_id": "YNAB:1234:1999-12-31:1",
+        "deleted": false,
+        "subtransactions": []
+      },
+      {
+        "id": "asdf1235-asdf-1235-asdf-1235asdf1235",
+        "date": "1999-12-31",
+        "amount": -12100,
+        "memo": null,
+        "cleared": "uncleared",
+        "approved": true,
+        "flag_color": null,
+        "account_id": "asdf1235-asdf-1235-asdf-1235asdf1235",
+        "account_name": "Asdf 1235",
+        "payee_id": "asdf1235-asdf-1235-asdf-1235asdf1235",
+        "payee_name": "Asdf 1234",
+        "category_id": "asdf1235-asdf-1235-asdf-1235asdf1235",
+        "category_name": "Asdf 1235",
+        "transfer_account_id": null,
+        "transfer_transaction_id": null,
+        "matched_transaction_id": null,
+        "import_id": "YNAB:1235:1999-12-31:1",
+        "deleted": false,
+        "subtransactions": []
+      }
+    ],
+    "server_knowledge": 54321
+  }
+}
+
+ */
